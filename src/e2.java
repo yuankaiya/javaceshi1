494ff922b995bfd8c7f760c625c07f05766c4447
zhaowodetuzi <2417569701@qq.com>
2019-05-14 17:07:59 +0800

    addc002

--- src/e2.java
+++ src/e2.java
@@ -2,5 +2,6 @@ public class e2 {
     public static void main(String[] args) {
         System.out.println("02");
         System.out.println("fen001ceshi001");
+        System.out.println("fen001ceshi002");
     }
 }
